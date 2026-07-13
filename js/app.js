d9c370bcabc0dcb0f644e3b1186e463d095eb3bf
bkpsdmdBangka <bkd.bangka.mutasi.2025@gmail.com>
2026-07-13 15:14:08 +0700

    Resolve merge conflict

---
+++ js/app.js
@@ -0,0 +1,30 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js';
+import { getFunctions } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-functions.js';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCauSkZHItjoqinvtkBt77Nq-9BXjDXOKA",
+  authDomain: "jptapp-672dd.firebaseapp.com",
+  projectId: "jptapp-672dd",
+  storageBucket: "jptapp-672dd.firebasestorage.app",
+  messagingSenderId: "66625982057",
+  appId: "1:66625982057:web:8b390c1164374225e89cf8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const functions = getFunctions(app, 'asia-southeast2');
+
+/**
+ * Hitung base URL halaman (untuk membangun link penilai)
+ * Contoh: https://user.github.io/PenilaianSekdaMakalah
+ */
+export function getBaseUrl() {
+  const { origin, pathname } = window.location;
+  // Hapus nama file terakhir dari path
+  const base = pathname.replace(/\/[^/]*$/, '');
+  return origin + base;
+}
